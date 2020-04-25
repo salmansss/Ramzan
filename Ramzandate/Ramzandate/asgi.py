4f0af3228d4bcf1097b85d89e6110eb7c804675b
salmansss <34932907+salmansss@users.noreply.github.com>
2020-04-25 21:24:29 +0530

    ramzan_date

---
+++ Ramzandate/Ramzandate/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Ramzandate project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ramzandate.settings')
+
+application = get_asgi_application()
---
+++ Ramzandate/ramzan/README.md
@@ -0,0 +1,2 @@
+# ramzan
+ramzan
